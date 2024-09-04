9b23fad4a03838ad42bb362085bc2c7cbc453508
abibflores <gon_abib@hotmail.com>
2024-09-04 10:28:08 -0600

    feat: Add SEO page with metadata and Card component

---
+++ src/components/trece/CardWithButton/CardWithButton.tsx
@@ -0,0 +1,19 @@
+
+'use client'
+import React from "react"
+import { Card } from "@/components/Card/Card";
+import { Character } from "@/types";
+import styles from "./CardWithButton.module.css";
+
+export const CardWithButton = ({ data }: { data: Character }) => {
+    const handleClick = () => {
+        console.log('Clicked')
+    }
+
+    return (
+        <div className={styles.container}>
+            <Card data={data} />
+            <button onClick={handleClick} className={styles.button}>Click me</button>
+        </div>
+    )
+}
