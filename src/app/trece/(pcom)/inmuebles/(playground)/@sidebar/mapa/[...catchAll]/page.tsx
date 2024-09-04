01fbbbfb8c403049e694d8bbee0688ee327dc999
abibflores <gon_abib@hotmail.com>
2024-09-04 17:40:10 -0600

    chore: Remove unused files and update CSS styles

--- src/app/trece/(pcom)/inmuebles/(playground)/@sidebar/mapa/[...catchAll]/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/@modal/page.tsx
-import styles from "./sidebar.module.css"
-export default function SidebarPage() {
-  return (
-    <div className={styles.container}>
-        <h3>SidebarPage</h3>
-    </div>
-  );
-}
