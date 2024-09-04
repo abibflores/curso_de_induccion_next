f3ade10139e59541a7ffd303cd8fc1ee9356edaf
abibflores <gon_abib@hotmail.com>
2024-09-04 17:39:04 -0600

    chore: Remove unused files and update CSS styles
    This commit removes unused files and updates CSS styles in the project. The files `src/app/trece/(pcom)/inmuebles/(playground)/@mapa/[catastral-id]/page.tsx`, `src/app/trece/(pcom)/inmuebles/(playground)/@mapa/mapa/[...catchAll]/mapa.module.css`, and `src/app/trece/(pcom)/inmuebles/(playground)/@mapa/mapa/default.tsx` are deleted. Additionally, the CSS styles in `src/components/trece/Maps/Maps.module.css` and `src/components/trece/Maps/Maps.tsx` are updated to remove unnecessary padding and margin properties.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

--- src/app/trece/(pcom)/inmuebles/(playground)/@mapa/[catastral-id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/@modal/page.tsx
-import styles from "./mapa.module.css"
-export default function MapaPage() {
-  return (
-    <div className={styles.container}>
-        <h3>Mapa</h3>
-    </div>
-  );
-}
