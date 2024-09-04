51ad00cd03c5a09e63203e69052b411bd7053b8d
abibflores <gon_abib@hotmail.com>
2024-09-04 09:55:34 -0600

    chore: Update Next.js configuration and dependencies
    This commit updates the Next.js configuration by adding a new `next.config.js` file and removing the old `next.config.mjs` file. It also updates the dependencies in the `package.json` file, adding `@ant-design/cssinjs` and `antd` packages. Additionally, it introduces a new CSS module file `page.module.css` and a new component `Card` with its corresponding CSS module file `Card.module.css`. These changes improve the project's configuration and introduce new functionality for displaying character data from the Rick and Morty API.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

---
+++ next.config.js
@@ -0,0 +1,54 @@
+
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+  
+  const customCorsHeaders = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, PUT, DELETE, OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type, Authorization',
+            },
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=604800', // Cache de una semana para JS y CSS
+            },
+          ],
+        },
+        {
+          source: '/public/(.*)',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=604800', // Cache de una semana para imágenes
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  const nextConfig = {
+    reactStrictMode: false,
+    images: {
+      domains: ['rickandmortyapi.com'],
+    },
+    ...customCorsHeaders,
+  };
+  
+  module.exports = withBundleAnalyzer(nextConfig);
+  
