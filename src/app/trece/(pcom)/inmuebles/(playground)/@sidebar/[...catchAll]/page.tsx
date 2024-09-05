67efae4e3839807b2a4e1e1f2c866d6c78384064
abibflores <gon_abib@hotmail.com>
2024-09-04 18:01:53 -0600

    feat: Update SidebarPage to fetch and display location data
    This commit updates the SidebarPage component in the inmuebles/(playground)/@sidebar directory. It adds an asynchronous fetch request to the Rick and Morty API to retrieve location data. The fetched data is then mapped and rendered as card items in the SidebarPage, displaying the name, type, and dimension of each location.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

--- src/app/trece/(pcom)/inmuebles/(playground)/@sidebar/[...catchAll]/page.tsx
+++ src/app/trece/(pcom)/inmuebles/(playground)/@sidebar/[...catchAll]/page.tsx
@@ -1,9 +1,17 @@
 // app/@modal/page.tsx
 import styles from "./sidebar.module.css"
-export default function SidebarPage() {
+export default async function SidebarPage(props: any) {
+  const data = await fetch('https://rickandmortyapi.com/api/location').then(res => res.json())
+  console.log(props, "params")
   return (
     <div className={styles.container}>
-        <h3>SidebarPage</h3>
+      { data.results.map((location: any) => (
+        <div className={styles.card_item} key={location.id}>
+          <h2 className={styles.card_item_name}>{location.name}</h2>
+          <p className={styles.card_item_type}>{location.type}</p>
+          <p className={styles.card_item_dimension}>{location.dimension}</p>
+        </div>
+      )) }
     </div>
   );
 }
